fd4775e2d6a5c3b4d63b3126bb1da4d3742a3bd1
Nikolas North <nikolasn09@gmail.com>
2020-07-22 17:54:12 -0400

    Create database using Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,13 +1,76 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'p6Jtit8o7h6M5tM7MtyAs4o46JiPm683'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    profile_img = db.Column(db.String(20), nullable=False, default='default.svg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Event', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}, '{self.email}'. '{self.img_file}')"
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date}')"
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filepath = db.Column(db.String(20))
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    events = db.relationship('Event', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f"Category('{self.name}')"
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"Category('{self.name}')"
+
+
+event_tags = db.Table(
+    'event_tags',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+)
+
+event_images = db.Table(
+    'event_images',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+    db.Column('image_id', db.Integer, db.ForeignKey('image.id'), primary_key=True)
+)
+
 
 @app.route("/")
 def index():
     return render_template('timeline.html')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -16,6 +79,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -27,6 +91,7 @@ def login():
         else:
             flash('Login unsuccessful. Please check username and password.', 'error')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
