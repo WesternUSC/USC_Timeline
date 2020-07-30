227c00172628c03a3b56eb135cd273bfa9a57429
Nikolas North <nikolasn09@gmail.com>
2020-07-30 14:16:59 -0400

    Remove ability to create accounts on the frontend

--- usctimeline/routes.py
+++ usctimeline/routes.py
@@ -15,24 +15,24 @@ def index():
     return render_template('timeline.html', events=events, month_name=month_name)
 
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('account'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(
-            username=form.username.data,
-            email=form.email.data,
-            password=hashed_password
-        )
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('account'))
-    return render_template('register.html', title='Register', form=form)
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('account'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(
+#             username=form.username.data,
+#             email=form.email.data,
+#             password=hashed_password
+#         )
+#         db.session.add(user)
+#         db.session.commit()
+#         login_user(user)
+#         flash(f'Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('account'))
+#     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
