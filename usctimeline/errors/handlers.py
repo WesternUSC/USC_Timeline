7be2bbb11bed7ac6761fe83b00a9bc2d30d23332
Nikolas North <nikolasn09@gmail.com>
2020-10-05 11:25:25 -0400

    Comment code in usctimeline/errors module

--- usctimeline/errors/handlers.py
+++ usctimeline/errors/handlers.py
@@ -5,9 +5,11 @@ errors = Blueprint('erros', __name__)
 
 @errors.app_errorhandler(404)
 def error_404(error):
+    """Returns rendered template for 404 page when 404 error occurs."""
     return render_template('errors/404.html'), 404
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
+    """Returns rendered template for 500 page when 500 error occurs."""
     return render_template('errors/500.html'), 500
