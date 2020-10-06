ef774b8609b904c311275023a79d639f24585d6f
Nikolas North <nikolasn09@gmail.com>
2020-10-06 10:42:44 -0400

    Add docstrings

--- usctimeline/events/forms.py
+++ usctimeline/events/forms.py
@@ -7,9 +7,31 @@ from usctimeline.models import Category, Tag
 from usctimeline.tags.forms import tag_query
 
 def category_query():
+    """Returns a Flask-SQLAlchemy query object for the Category model."""
     return Category.query
 
 class EventForm(FlaskForm):
+    """Creates a form for creating a new Event object.
+
+    Attributes:
+        title:
+            An input element of type text for the event title.
+        date:
+            An input element of type date for the event date.
+        description:
+            An input element of type text for the event description.
+        external_url:
+            An input element of type url for the event 'learn more' button.
+        category:
+            A select element for the event category.
+        tags:
+            A select element with multiple attribute for the event tag(s).
+        images:
+            An input element of type file for the event image(s).
+        submit:
+            An input element of type submit to submit the form.
+    """
+
     title = StringField('Title', validators=[DataRequired()])
     date = DateField(
         'Date',
@@ -36,6 +58,22 @@ class EventForm(FlaskForm):
 
 
 def update_event_form_factory(default_category_name, event_id):
+    """Returns a new instance of UpdateEventForm.
+
+    Creates a new instance of UpdateEventForm. This class inherits EventForm
+    and overwrites the category and tags fields of an existing Event.
+
+    Args:
+        default_category_name:
+            Category name of an existing Event. This category name will be
+            pre-selected in the category select element.
+        event_id:
+            ID of an existing event. Needed to retrieve all tag(s) (if any) for
+            the Event of interest. If any tag(s) belong to the Event, then they
+            will be pre-selected in the tags select element.
+    Returns:
+        An instance of UpdateEventForm.
+    """
     class UpdateEventForm(EventForm):
         category = QuerySelectField(
             'Update Category',
@@ -58,6 +96,29 @@ def update_event_form_factory(default_category_name, event_id):
 
 
 class SearchEventForm(FlaskForm):
+    """Creates a form for searching specific event(s) in the database.
+
+    Attributes:
+        title:
+            An input element of type text for the event title.
+        exact_date:
+            An input element of type date. Represents a day on which an event
+            occurred.
+        from_date:
+            An input element of type date. Represents a day on which an event
+            occurred on or after. Meant to provide a date range in conjunction
+            with to_date field.
+        to_date:
+            An input element of type date. Represents a day on which an event
+            occurred on or before. Meant to provide a date range in conjunction
+            with from_date field.
+        category:
+            A select element for the event category.
+        tags:
+            A select element with multiple attribute for the event tag(s).
+        submit:
+            An input element of type submit to submit the form.
+    """
     title = StringField('Title')
     exact_date = DateField(
         'On This Exact Day:',
