7a77d96bfb19c11bda246d4e5577a0e1170a57c7
Nikolas North <nikolasn09@gmail.com>
2020-10-14 11:49:25 -0400

    Allow input of usrname, email, pwd

--- createuser.py
+++ createuser.py
@@ -1,14 +1,39 @@
-import os
+"""Script for creating a new user account.
+
+The script can be executed by typing in: `python createuser.py` (assuming your
+current directory is `USC_Timeline`).
+
+Upon executing the script, you will be prompted to enter a username, email and
+password. Using this information a new User will be instantiated and stored in
+the database.
+"""
+
+import sys
+import json
+from getpass import getpass
 from usctimeline import create_app, db, bcrypt
 from usctimeline.models import User
 
-app = create_app()
-with app.app_context():
-    password = bcrypt.generate_password_hash(os.environ.get('USC_PASSWORD')).decode('utf-8')
-    user = User(
-        username='Nikolas',
-        email='webdevelopment.intern@westernusc.ca',
-        password=password
+def create_user(username, email, password):
+    new_user = User(
+        username=username,
+        email=email,
+        password=bcrypt.generate_password_hash(password).decode('utf-8')
     )
-    db.session.add(user)
+    db.session.add(new_user)
     db.session.commit()
+
+def main():
+    username = input("Username: ")
+    email = input("Email: ")
+    password = getpass("Password: ")
+    password_verification = getpass("Confirm Password: ")
+    if password == password_verification:
+        create_user(username, email, password)
+    else:
+        print("Error: Passwords did not match. Please try again.")
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        main()
